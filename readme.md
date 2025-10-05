bcf99caad6df5f672102440456275e6388e143c8
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-10-05 17:46:16 +0200

    Implement ESLint and prettier

---
+++ readme.md
@@ -0,0 +1,13 @@
+## npm installs
+
+# dependencies
+npm install express
+npm install eslint @eslint/js prettier eslint-config-prettier eslint-plugin-prettier -D
+
+## Formatting issues command
+
+npm run lint
+npm run lint:fix
+
+npm run format
+npm run format:check
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 // running the server implementing some logging and everything else
 
-import app from "./app.js";
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
